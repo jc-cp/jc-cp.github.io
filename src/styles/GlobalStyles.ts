d9d5d4ba49ce8d33c994c9ddd3e2bb0ef7a33ffd
JuanquiWLS <jc.climentpardo@gmail.com>
2024-11-13 16:07:01 +0100

    new deployment x3
    Signed-off-by: JuanquiWLS <jc.climentpardo@gmail.com>

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,14 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
-    background: ${({ theme }) => theme.colors.background || '#fff'};
-    color: ${({ theme }) => theme.colors.text || '#000'};
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text.primary};
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   #root {
     min-height: 100vh;
+    width: 100%;
+  }
+
+  /* Remove any Vite template styles */
+  :root {
+    color-scheme: dark;
   }
 `; 
