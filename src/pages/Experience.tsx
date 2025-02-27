1f70df394f52465d99783458d83ecaf7697b5d1f
JuanquiWLS <jc.climentpardo@gmail.com>
2025-02-27 11:41:24 +0100

    started experiences
    Signed-off-by: JuanquiWLS <jc.climentpardo@gmail.com>

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,19 +1,40 @@
-import styled from "styled-components";
-
-const PageTitle = styled.h1`
-  font-family: 'Poppins', sans-serif;
-  font-size: 2.5rem;
-  font-weight: 700;
-  background: linear-gradient(45deg, #FF6B6B, #4ECDC4);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  margin-bottom: ${({ theme }) => theme.spacing.xl};
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
+import { PageTitle, PageContainer, Timeline, TimelineItem, Card, LogoContainer, ContentContainer, Title, Subtitle, Period, Description, Tags, Tag, AchievementsList, InteractiveLink } from "../components/SharedStyles";
+import fraunhoferLogo from "../assets/experience/fraunhofer.jpg";
 
 export const Experience = () => {
   return (
+    <PageContainer>
       <PageTitle>Experience</PageTitle>
+      <Timeline>
+        <TimelineItem>
+          <Card>
+            <LogoContainer zoom={100}>
+              <img src={fraunhoferLogo} alt="Fraunhofer Logo" />
+            </LogoContainer>
+            <ContentContainer>
+              <Title>Data Science Engineer</Title>
+              <Subtitle>Fraunhofer Institute for New Drive Systems (NAS-ICT)</Subtitle>
+              <Period>2019 - 2020</Period>
+              <Description>
+                Developed a data pipeline for the analysis of electrical vehicle data. Through UDP protocol, the data was collected from the charging station and was logged into a an SQL database. Visualized the data with Python and Grafana.
+              </Description>
+              <Tags>
+                <Tag>Software Engineering</Tag>
+                <Tag>Cloud Computing</Tag>
+                <Tag>Machine Learning</Tag>
+              </Tags>
+              <AchievementsList>
+                <li>
+                  Project:
+                  <InteractiveLink to="/projects#google-search">
+                    Google Search
+                  </InteractiveLink>
+                </li>
+              </AchievementsList>
+            </ContentContainer>
+          </Card>
+        </TimelineItem>
+      </Timeline>
+    </PageContainer>
   );
 }; 
